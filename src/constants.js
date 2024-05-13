954780494f004c85969bea6b9d9b3981328fe992
James Smith <james.smith@djalbat.com>
2024-05-13 17:08:01 +0100

    Cleared out the constants.

--- src/constants.js
+++ src/constants.js
@@ -2,23 +2,13 @@
 
 export const { PI } = Math;
 
-export const HASH = "#";
-export const GRID = "grid";
 export const TAP_DELAY = 250;
 export const DRAG_DELAY = TAP_DELAY;  ///
 export const PINCH_DELAY = TAP_DELAY; ///
 export const PI_OVER_TWO = PI / 2;
-export const EMPTY_STRING = "";
-export const SCROLL_DELAY = 10;
-export const DECELERATION = 0.0333333;
-export const UP_DIRECTION = +1;
-export const DOWN_DIRECTION = -1;
 export const MAXIMUM_SPREAD = PI / 4;
-export const MENU_ZOOM_RATIO = 1.1;
 export const MAXIMUM_TAP_TIME = 125;
-export const BACKGROUND_COLOUR = "background-color"; ///
 export const MINIMUM_SWIPE_SPEED = 1;
 export const PORTRAIT_ORIENTATION = "portrait";
 export const LANDSCAPE_ORIENTATION = "landscape";
-export const OPEN_MENU_TAP_AREA_HEIGHT = 128;
 export const ORIENTATION_PORTRAIT_QUERY = "(orientation: portrait)";
